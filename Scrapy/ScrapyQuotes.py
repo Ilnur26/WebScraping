b2e4011ac6af63bed1d4e8cd3f1435791c80079b
ilnur26 <ilnur26@gmail.com>
2023-06-28 19:06:10 +0600

    moving to the next page

--- Scrapy/ScrapyQuotes.py
+++ Scrapy/ScrapyQuotes.py
@@ -13,7 +13,14 @@ class ScratchQuotes(scrapy.Spider):
                  'author': author
              }
 
-        if response.css('li.next').get():
-            print('\n\n\n\n NEXT button available')
+        nextPageUrl = response.css('li.next a::attr(href)').get()
+        print(nextPageUrl)
+
+        # if response.css('li.next').get():
+        if nextPageUrl:
+            yield response.follow(nextPageUrl, callback=self.parse)
+            # print('\n\n\n\n NEXT button available')
         else:
             print('\n\n\n\n LAST page')
+
+# nextPageUrl -> /page/2
